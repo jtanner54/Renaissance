e5a34e26bba26a56b5025f8f69a7e5fd9650684a
tannerje <jtanner0786@gmail.com>
2025-12-27 20:31:41 -0600

    navbar redid with heavy influence from ran

--- renaissance/src/Components/NavBar.tsx
+++ renaissance/src/Components/NavBar.tsx
@@ -1,10 +1,44 @@
-import DropdownButton from '@/src/Components/DropdownButton';
+'use client'
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import {IoHomeOutline } from "react-icons/io5";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+import { FaWrench } from "react-icons/fa";
 
 export default function NavBar() {
+    const location = usePathname();
+
     return (
-        <div className="dark:bg-black">
-            <div className=''></div>
-            <DropdownButton />
-        </div>
+        <header className="fixed flex justify-center pointer-events-none top-4 left-0 right-0">
+            <nav className="pointer-events-auto overflow-hidden max-w-[95vw]">
+                <ul className="flex p-3 dark:bg-black border border-white/7 rounded-full gap-3 overflow-x-auto scrollbar-hide">
+                    <li>
+                        <Link
+                        href="/"
+                        className={`flex p-2.5 gap-2.5 justify-center items-center border border-white/7 rounded-full ${location == '/' ? 'bg-white/24' : 'bg-zinc-100/5'}`}>
+                            <IoHomeOutline className="size-4.5"/>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                        href="/About"
+                        className={`flex p-2.5 gap-2.5 justify-center items-center border border-white/7 rounded-full ${location == '/About' ? 'bg-white/24' : 'bg-zinc-100/5'}`}>
+                            <IoIosInformationCircleOutline className="size-4.5"/>
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                        href="/Tech"
+                        className={`flex p-2.5 gap-2.5 justify-center items-center border border-white/7 rounded-full ${location == '/Tech' ? 'bg-white/24' : 'bg-zinc-100/5'}`}>
+                            <FaWrench className="size-4.5"/>
+                            Tech
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 }
--- renaissance/src/app/page.tsx
+++ renaissance/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
     return (
-        <div className="text-6xl flex min-h-screen items-center justify-center dark:bg-black">
+        <div className="text-6xl flex min-h-screen items-center justify-center dark:bg-graphite">
             <h1>Jack Tanner</h1>
         </div>
     );
